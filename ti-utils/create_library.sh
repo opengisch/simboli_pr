c8c85f2fc14108c5221ae87cbd79a3ddfbe770ae
Mario Baranzini <mario@opengis.ch>
2018-11-21 12:20:49 +0100

    Improve library generation

---
+++ ti-utils/create_library.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Remove old svg because they are not overwritten
+rm ../result/libreria/svg/*
+
+# Create library.xml and svg files with patch applied
+python library.py
+
+# Remove svg path from library.xml
+sed -i -- 's/\/home\/mario\/workspace\/repos\/symbology-ti\/result\/libreria\/svg\///g' ../result/libreria/libreria.xml
+
+# Fix very thin lines
+sed -i -- 's/<prop k="line_width" v="0.0352778"/<prop k="line_width" v="0"/g' ../result/libreria/libreria.xml   
+sed -i -- 's/<prop k="outline_width" v="0.0352778"/<prop k="line_width" v="0"/g' ../result/libreria/libreria.xml
+
+# Remove emf files
+rm ../result/libreria/svg/*.emf
